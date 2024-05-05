83de81c248ed71ce012c0da33002e7dc5ed09121
nick <nickdsmith@outook.com>
2024-05-04 18:13:18 -0600

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import json
+import requests
+from flask import Flask, request, jsonify
+
+class State:
+    def __init__(self):
+        self.country = ""
+        self.state = ""
+        self.
+        
+        
+class Profile:
+    def __init__(self):
+        self.name = ""
+        self.age = 0
+        self
+        
+        
